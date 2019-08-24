033af524ad2cf2f5e59411e5b467296402dd38ba
dev <dev@dev.io>
2019-08-24 12:24:07 +0900

    update.

--- public/ip.php
+++ public/ip.php
@@ -1,4 +1,9 @@
 <?php
 header('Access-Control-Allow-Origin:*');
 header("Content-Type: text/plain; charset=UTF-8");
-echo $_SERVER['REMOTE_ADDR'];
+
+if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    echo $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    echo $_SERVER['REMOTE_ADDR'];
+}
